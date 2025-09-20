b93f18b56a484a9f009d1a93273b5c0bf3a55ca6
Diego Gettatelli <dgettatelli@gmail.com>
2025-09-20 18:07:27 +0200

    feat: add background image

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,19 @@ import 'package:frontend/game/GameInitLoadingScreen.dart';
 import 'package:frontend/game/gambit_game.dart';
 
 void main() {
-  runApp(GameWidget(
+  runApp(
+      GameWidget(
       game: GambitGame(),
       loadingBuilder: (BuildContext ctx) {
         return const Gameinitloadingscreen();
+      },
+      backgroundBuilder: (BuildContext ctx) {
+        return Image.asset(
+          'assets/images/background.jpg',
+          fit: BoxFit.cover,
+          width: double.infinity,
+          height: double.infinity,
+        );
       },
   ));
 }
